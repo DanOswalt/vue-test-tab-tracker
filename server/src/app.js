c26132e227bb2380e141966af809aa3156e1112e
Dan Oswalt <do189705@gmail.com>
2018-02-11 19:49:21 -0800

    end of vid 1, working as it should now

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const port = process.env.PORT || 8081
 
 const app = express()
 app.use(morgan('combined'))
@@ -9,7 +10,9 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/register', (req, res) => {
-  res.send({ message: `${req.body.email}` })
+  res.send({ message: `${req.body.email} was registered!` })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(port, () => {
+  console.log('connected at', port)
+})
